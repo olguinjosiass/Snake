0b5939d609624aa05163d65a8b2e6bdbe4f6debf
olguinjosiass <93270408+olguinjosiass@users.noreply.github.com>
2022-03-13 15:36:46 -0300

    Update KeyboardListener.java

--- src/KeyboardListener.java
+++ src/KeyboardListener.java
@@ -5,22 +5,22 @@ import java.awt.event.KeyEvent;
  	
  		public void keyPressed(KeyEvent e){
  		    switch(e.getKeyCode()){
-		    	case 39:	// -> Right 
-		    				//if it's not the opposite direction
+		    	case 39:	// -> Derecha
+		    				
 		    				if(ThreadsController.directionSnake!=2) 
 		    					ThreadsController.directionSnake=1;
 		    			  	break;
-		    	case 38:	// -> Top
+		    	case 38:	// -> Arriba
 							if(ThreadsController.directionSnake!=4) 
 								ThreadsController.directionSnake=3;
 		    				break;
 		    				
-		    	case 37: 	// -> Left 
+		    	case 37: 	// -> Izquierda 
 							if(ThreadsController.directionSnake!=1)
 								ThreadsController.directionSnake=2;
 		    				break;
 		    				
-		    	case 40:	// -> Bottom
+		    	case 40:	// -> Abajo
 							if(ThreadsController.directionSnake!=3)
 								ThreadsController.directionSnake=4;
 		    				break;
@@ -29,4 +29,4 @@ import java.awt.event.KeyEvent;
  		    }
  		}
  	
- }
+
